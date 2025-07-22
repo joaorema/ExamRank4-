0cb28be9d0f7b398c0e2aa4e77ecc55ab31a8c2f
joaorema <joaopcrema@gmail.com>
2025-07-22 15:54:25 +0100

    new versions of picoshell, popen, sandbox

---
+++ lvl1/picoshell/teste.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int picoshell(char **cmds[])
+{
+    int status = 0;
+    int i = -1;
+    pid_t pid;
+    int result = 0;
+    int fd[2];
+    int fd_stdin = 0;
+
+    while(cmds[i++])
+    {
+        if(cmds[i + 1])
+        {
+            if(pipe(fd) == -1)
+                return -1;
+        }
+        else
+        {
+            fd[0] = -1;
+            fd[1] = -1;
+        }
+        pid = fork();
+        if(pid == -1)
+        {
+            close(fd[0]);
+            close(fd[1]);
+            close(fd_stdin);
+            return 1;
+        }
+        if(pid == 0)
+        {
+            if(fd_stdin)
+            {
+                if(dup2(fd_stdin, 0) == -1)
+                    exit(1);
+                close(fd_stdin);
+            }
+            if(fd[1] != -1)
+            {
+                if(dup2(fd[1], 1) == -1)
+                    exit(1);
+                close(fd[0]);
+                close(fd[1]);
+            }
+            execvp(cmds[i][0], cmds[i]);
+            exit(1);
+        }
+        if(fd_stdin != 0)
+            close(fd_stdin);
+        if(fd[1] != 0)
+            close(fd[1]);
+        fd_stdin = fd[0];
+    }
+    while(wait(&status) > 0 )
+    {
+        if(WIFEXITED(status) && WEXITSTATUS(status) != 0)
+            result = 1;
+        else if(!WIFEXITED(status))
+            result = 1;
+    }
+    return (result);
+}
